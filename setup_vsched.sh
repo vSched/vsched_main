e89369cbe7b5d8e44270e447cf23bcba70e5b064
Edward <eguo3@pride.hofstra.edu>
2024-10-15 23:01:48 -0400

    updated submodules and changed setup

--- setup_vsched.sh
+++ setup_vsched.sh
@@ -1,5 +1,5 @@
+#!/bin/bash
 
-#for probers
 sudo echo "+cpu" | sudo tee /sys/fs/cgroup/cgroup.subtree_control;
 sudo echo "+cpuset" | sudo tee /sys/fs/cgroup/cgroup.subtree_control;
 
@@ -19,16 +19,25 @@ sudo echo "threaded" > /sys/fs/cgroup/hi_prgroup/cgroup.type;
 sudo echo 1 | sudo tee /sys/fs/cgroup/lw_prgroup/cpu.idle
 sudo echo -20 | sudo tee /sys/fs/cgroup/hi_prgroup/cpu.weight.nice
 
-sudo make
+#sudo make
 
+
+(cd ./vsched_kernel/vtopology/ && sudo make)
+
+(cd ./vsched_kernel/vcapacity/ && sudo make)
 
 (cd ./vsched_kernel/custom_modules/ && sudo make)
 
 sudo insmod ./vsched_kernel/custom_modules/preempt_proc.ko
 
-(cd ./vsched_kernel/tools/bpf && sudo make)
+(cd ./vsched_kernel/tools/lib/bpf && sudo make)
 
-sudo ./vsched_kernel/tools/bpf/bpftool/bpftool btf dump file /sys/kernel/btf/vmlinux format c > ./vsched_kernel/tools/bpf/vcfs/vmlinux.h
+(cd ./vsched_kernel/tools/bpf/bpftool && sudo make)
+
+sudo bash -c '
+cd ./vsched_kernel/tools/bpf/vcfs && 
+../bpftool/bpftool btf dump file /sys/kernel/btf/vmlinux format c > vmlinux.h
+'
 
 (cd ./vsched_kernel/tools/bpf/vcfs && sudo make)
 
